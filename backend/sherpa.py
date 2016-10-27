af61d9b4dd50da7277d7ca5e0953b9adbb8e7eac
Adam Bain <bainada.iit@gmail.com>
2016-10-27 17:04:01 -0500

    Dummy socket app

---
+++ backend/sherpa.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=None)
+thread = None
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        socketio.sleep(10)
+        count += 1
+        socketio.emit('my_response',
+                      {'data': 'Server generated event', 'count': count})
+
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
+
+@socketio.on('connect')
+def test_connect():
+    global thread
+    if thread is None:
+        thread = socketio.start_background_task(target=background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
+@socketio.on('my_event')
+def test_message(message):
+    emit('my_response', {'data': 'got it!', 'count': 42})
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
