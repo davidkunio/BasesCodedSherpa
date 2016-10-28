c75965eeebf5cdf246855a177cc846859cfa3eea
davidkunio <davidkunio@gmail.com>
2016-10-28 07:39:18 -0500

    print rows

--- backend/test_statcast.py
+++ backend/test_statcast.py
@@ -4,6 +4,6 @@ game = StatCastData()
 
 with open("data_output.txt",'w') as f:
     for x in range(game.get_total_items()):
-        d = str(game.return_update())
+        d = str(game.return_update())+'\n'
         f.write(d)
         print(d)
