475a0349d574d99af5fff5b9092d5097630ca7e4
Adam Bain <bainada.iit@gmail.com>
2016-10-28 10:53:30 -0500

    sherpa

--- backend/sherpa.py
+++ backend/sherpa.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-# from flask_cors import CORS
+from flask_cors import CORS
 
 from time import sleep
 import random
@@ -9,15 +9,20 @@ import logging
 
 from handlers import handlers
 from statcastdata import StatCastData
-import sys
+
+import json
 
 app = Flask(__name__)
 
 # logging.getLogger('flask_cors').level = logging.DEBUG
 
+#file shirt
+f = open("message_dump.json","w")
+f.write("[")
+
 app.config['SECRET_KEY'] = 'secret!'
-# CORS(app, resources='/*')
-socketio = SocketIO(app)
+CORS(app, resources='/*')
+socketio = SocketIO(app, )
 
 @app.route('/')
 def index():
@@ -43,12 +48,16 @@ def background_thread():
 
             for message in sherpa_messages:
                 socketio.emit('sherpa_message', message)
+                f.write(json.dumps(message)+',')
 
-        sleep(random.uniform(.5,1))
 
+
+        #sleep(0.1)
+
+
+thread = socketio.start_background_task(target=background_thread)
 
 
 if __name__ == '__main__':
-    thread = socketio.start_background_task(target=background_thread)
 
-    socketio.run(app, host="ec2-54-196-57-249.compute-1.amazonaws.com", port=5000, debug=True)
+    socketio.run(app, host="127.0.0.1", port=5000, debug=False)
