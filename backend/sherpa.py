f3849e747188da59a2b2eec0454d23b2c7b4ac9e
Adam Bain <bainada.iit@gmail.com>
2016-10-28 09:34:17 -0500

    going back

--- backend/sherpa.py
+++ backend/sherpa.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-from flask_cors import CORS
+# from flask_cors import CORS
 
 from time import sleep
 import random
@@ -13,7 +13,7 @@ import sys
 
 app = Flask(__name__)
 
-logging.getLogger('flask_cors').level = logging.DEBUG
+# logging.getLogger('flask_cors').level = logging.DEBUG
 
 app.config['SECRET_KEY'] = 'secret!'
 # CORS(app, resources='/*')
@@ -49,16 +49,6 @@ def background_thread():
 
 
 if __name__ == '__main__':
-    try:
-        host, port = sys.argv[1], int(sys.argv[2])
-    except Exception as e:
-        host = "ec2-54-196-57-249.compute-1.amazonaws.com"
-        port = 5000
-
-    print("Running on host:{} port:{}".format(host, port))
     thread = socketio.start_background_task(target=background_thread)
 
-    # import eventlet
-    # import eventlet.wsgi
-    # eventlet.wsgi.server(eventlet.listen((host, 5000)), app)
-    socketio.run(app, host=host, port=port, debug=True)
+    socketio.run(app, host="ec2-54-196-57-249.compute-1.amazonaws.com", port=5000, debug=True)
