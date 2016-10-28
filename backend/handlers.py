9dff921e8c9acc5401526b63ff2ccf6d1202ab81
davidkunio <davidkunio@gmail.com>
2016-10-28 06:39:59 -0500

    add handler

--- backend/handlers.py
+++ backend/handlers.py
@@ -1,5 +1,7 @@
 from winpercent import win_p_and_li
+from NameLookup import NameLookup
 
+name = NameLookup()
 handlers = []
 
 def register(f):
@@ -8,13 +10,13 @@ def register(f):
 
 
 @register
-def in_play(before, event, after, index):
-    if event and 'isInPlay' in event and event['isInPlay']:
+def in_play(before, event, after):
+    if 'isInPlay' in event and event['isInPlay']:
         print("returning from in_play")
-        return {"title": "In Play", "text": "The ball is in play", "index": index}
+        return {"title": "In Play", "text": "The ball is in play"}
 
 @register
-def big_play(before, event, after, index):
+def big_play(before, event, after):
     win_p_before, _ = win_p_and_li(before)
     win_p_after, _ = win_p_and_li(after)
 
@@ -22,65 +24,65 @@ def big_play(before, event, after, index):
         print("BIG PLAY HOME")
         return {
             "title": "Big Play",
-            "index": index,
             "text": "That play was huge, the home team has improved from a "
             "{:.3}% to a {:.3}% chance of winning.".format(100*win_p_before, 100*win_p_after)}
     if win_p_after-win_p_before < -.10:
         print("BIG PLAY AWAY")
         return {
             "title": "Big Play",
-            "index": index,
             "text": "That play was huge, the away team has improved from a "
             "{:.3}% to a {:.3}% chance of winning.".format(100*(1-win_p_before), 100*(1-win_p_after))
         }
 
 @register
-def high_leverage(before, event, after, index):
+def high_leverage(before, event, after):
     _, li = win_p_and_li(after)
-    print("LI: {}".format(li))
     if li > 2.5:
         print("HIGH LEVERAGE")
-        return {"title": "Big Opportunity", "index": index, "text": "Head's up, this could be big. With a hit, this game could change."}
+        return {"title": "Big Opportunity", "text": "Head's up, this could be big. With a hit, this game could change."}
 
-# @register
-# def new_batter(before, event, after):
-#     pass
-#
-# @register
-# def new_runner(before, event, after):
-#     pass
-#
-# @register
-# def new_pitcher(before, event, after):
-#     pass
+@register
+def new_batter(before, event, after):
+    if 'new_batter' in before and before['new_batter'] == 1:
+        print('New Batter')
+        return("title")
+    pass
 
-# @register
-# def starter_high_pitch_count(before, event, after):
-#     pass
-#
-# @register
-# def starter_low_pitch_count(before, event, after):
-#     pass
-#
-# @register
-# def bunt_situation(before, event, after):
-#     pass
-#
-# @register
-# def hit_and_run_situation(before, event, after):
-#     pass
-#
-# @register
-# def steal_situation(before, event, after):
-#     pass
-#
-# @register
-# def intentional_walk_situation(before, event, after):
-#     pass
-#
-# @register
-# def pinch_hitter_situation(before, event, after):
-#     pass
+@register
+def new_runner(before, event, after):
+    pass
+
+@register
+def new_pitcher(before, event, after):
+    pass
+
+@register
+def starter_high_pitch_count(before, event, after):
+    pass
+
+@register
+def starter_low_pitch_count(before, event, after):
+    pass
+
+@register
+def bunt_situation(before, event, after):
+    pass
+
+@register
+def hit_and_run_situation(before, event, after):
+    pass
+
+@register
+def steal_situation(before, event, after):
+    pass
+
+@register
+def intentional_walk_situation(before, event, after):
+    pass
+
+@register
+def pinch_hitter_situation(before, event, after):
+    pass
 #
 # @register
 # def hit_and_run_situation(before, event, after):
