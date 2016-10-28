62fb1b8a6c708bc908380bb5cebe59585892ca22
Adam Bain <bainada.iit@gmail.com>
2016-10-28 06:54:37 -0500

    comment not compliant handlers

--- backend/handlers.py
+++ backend/handlers.py
@@ -54,42 +54,42 @@ def new_batter(before, event, after, index):
                 "index": index,
                 "text":"{} strides up to the plate.".format(name.return_name(before['batter']))}
 
-@register
-def new_runner(before, event, after):
-    pass
-
-@register
-def new_pitcher(before, event, after):
-    pass
-
-@register
-def starter_high_pitch_count(before, event, after):
-    pass
-
-@register
-def starter_low_pitch_count(before, event, after):
-    pass
-
-@register
-def bunt_situation(before, event, after):
-    pass
-
-@register
-def hit_and_run_situation(before, event, after):
-    pass
-
-@register
-def steal_situation(before, event, after):
-    pass
-
-@register
-def intentional_walk_situation(before, event, after):
-    pass
-
-@register
-def pinch_hitter_situation(before, event, after):
-    pass
+# @register
+# def new_runner(before, event, after):
+#     pass
 #
+# @register
+# def new_pitcher(before, event, after):
+#     pass
+#
+# @register
+# def starter_high_pitch_count(before, event, after):
+#     pass
+#
+# @register
+# def starter_low_pitch_count(before, event, after):
+#     pass
+#
+# @register
+# def bunt_situation(before, event, after):
+#     pass
+#
+# @register
+# def hit_and_run_situation(before, event, after):
+#     pass
+#
+# @register
+# def steal_situation(before, event, after):
+#     pass
+#
+# @register
+# def intentional_walk_situation(before, event, after):
+#     pass
+#
+# @register
+# def pinch_hitter_situation(before, event, after):
+#     pass
+# #
 # @register
 # def hit_and_run_situation(before, event, after):
 #     pass
